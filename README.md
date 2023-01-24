82367940b1b72d0e690c18e395b13c12bd51a207
Alex Morris <alex.morris@catalyst.net.nz>
2023-01-25 10:37:02 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# moodle-filter_forumembed
+
+![GitHub Workflow Status (main)](https://img.shields.io/github/actions/workflow/status/catalyst/moodle-filter_forumembed/ci.yml?branch=main)
+
+Embed forum discussions anywhere with filter tags.
+
+* [Branches](#branches)
+* [Branches](#branches)
+* [Warm thanks](#warm-thanks)
+
+## Branches
+
+| Moodle version | Totara version | Branch                                                                 | PHP  |
+|----------------|----------------|------------------------------------------------------------------------|------|
+| Moodle 3.9 +   | Totara 12      | [main](https://github.com/catalyst/moodle-filter_forumembed/tree/main) | 7.2+ |
+
+## Warm thanks
+
+Thanks to [Massey University](https://www.massey.ac.nz/) for funding the initial development of this plugin.
+
+This plugin was developed by [Catalyst IT](https://www.catalyst.net.nz/).
+
+<img alt="Catalyst IT" src="https://raw.githubusercontent.com/catalyst/moodle-auth_saml2/MOODLE_39_STABLE/pix/catalyst-logo.svg" width="400">
---
+++ version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of FilterCodes for Moodle - http://moodle.org/
+//
+// FilterCodes is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FilterCodes is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for forum discussion embed.
+ *
+ * @package     filter_filtercodes
+ * @author      Alex Morris <alex.morris@catalyst.net.nz>
+ * @copyright   Catalyst IT
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'filter_forumembed';
+$plugin->version   = 2022052400;
+$plugin->release   = '2022052400';
+$plugin->requires  = 2020061513;
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->supported = [39, 401];
